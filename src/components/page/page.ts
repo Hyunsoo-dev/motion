795ef4174ceba5374876b1d1fd814758e24cd1c3
Hyunsoo-dev <jing07161@gmail.com>
2022-07-03 22:08:12 +0900

    Feature: Base component, image Component 추가

---
+++ src/components/page/page.ts
@@ -0,0 +1,7 @@
+import { BaseComponent } from "../component.js";
+export class PageComponent extends BaseComponent<HTMLUListElement> {
+  constructor() {
+    const htmlString = `<ul class="page">This is Page Component"`;
+    super(htmlString);
+  }
+}
